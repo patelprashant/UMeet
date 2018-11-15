7cf6e9fa2e754fea84d94735f09f62ed0f564972
Prashant <prashant.patel77@gmakil.com>
2018-11-15 22:33:10 +1100

    Added People model attributes and PeoplDao mehtods with Livedata support

--- app/src/main/java/com/example/umeet/data/db/PeopleDao.kt
+++ app/src/main/java/com/example/umeet/data/db/PeopleDao.kt
@@ -1,4 +1,33 @@
 package com.example.umeet.data.db
 
-class PeopleDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.umeet.data.model.People
+
+@Dao
+interface PeopleDao {
+    // Queries
+
+    // Select All
+    @Query("SELECT * FROM People ORDER BY id DESC")
+    fun getAll(): LiveData<List<People>>
+
+    // Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(people: People)
+
+    // Delete All
+    @Query("DELETE FROM People")
+    fun deleteAll()
+
+    // Select by id
+    @Query("SELECT * FROM People WHERE id = :id")
+    fun findById(id: Int): LiveData<People>
+
+    // Select by name
+    @Query("SELECT * FROM People WHERE name LIKE '%' || :name || '%'  ")
+    fun findByName(name: String): LiveData<List<People>>
 }
--- app/src/main/java/com/example/umeet/data/model/People.kt
+++ app/src/main/java/com/example/umeet/data/model/People.kt
@@ -1,4 +1,14 @@
 package com.example.umeet.data.model
 
-class People {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class People(
+    var name: String = "",
+    var metAt: String = "",
+    var contact: String = "",
+    var eMail: String = "",
+    var mobile: String = "",
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+)

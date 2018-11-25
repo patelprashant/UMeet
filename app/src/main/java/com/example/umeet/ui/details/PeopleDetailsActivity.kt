f5c180f45e1db7db314a70a7f2c2ecea160fed12
Prashant <prashant.patel77@gmakil.com>
2018-11-25 19:00:49 +1100

    Added Peple details activity, fragment and layout

---
+++ app/src/main/java/com/example/umeet/ui/details/PeopleDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.umeet.ui.details
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umeet.R
+
+class PeopleDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_people_details)
+    }
+}

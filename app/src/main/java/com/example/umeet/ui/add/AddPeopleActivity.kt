e8d0d31997221a0a4e3e6b2bbe84b5a35c5df268
Prashant <prashant.patel77@gmakil.com>
2018-11-28 07:45:05 +1100

    Added People Add activity, fragment and layout

---
+++ app/src/main/java/com/example/umeet/ui/add/AddPeopleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.umeet.ui.add
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umeet.R
+
+class AddPeopleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_people)
+    }
+}
